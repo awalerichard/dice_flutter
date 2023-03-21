d6677cb68e9dba0d60bb598c6ae0da4c337d6670
awalerichard <richardawale182@gmail.com>
2023-03-21 19:48:56 +0200

    functionality added for button (math.random)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,26 +17,42 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = Random().nextInt(6) + 1;
+  int rightDiceNumber = Random().nextInt(6) + 1;
+
   @override
   Widget build(BuildContext context) {
+    //int rightDiceNumber = Random().nextInt(5) + 1;
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child: TextButton(
               onPressed: () {
-                print("left button got pressed");
+                print('dice number = $leftDiceNumber');
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                  //print('dice number = $leftDiceNumber');
+                });
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
             child: TextButton(
               onPressed: () {
-                print("Right button got pressed");
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                  //print('dice number = $rightDiceNumber');
+                });
               },
-              child: Image.asset('images/dice2.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
